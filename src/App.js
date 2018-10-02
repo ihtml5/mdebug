1e6b6e919667fc38f408be09646ce0cd846589e8
thinkchen <phpython8@gmail.com>
2018-10-02 17:29:25 +0800

    fix master

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import '@/App.css';
-import JSONTree from 'react-json-tree';
 import { initData } from '@/mock';
 import Inspector from 'react-inspector';
 import { MdebugHeader, MdebugApplication } from '@/modules';
@@ -11,54 +10,62 @@ class App extends Component {
     this.state = {
       showDebug: true,
     };
-		this._times = 1;
-		this._lastTapTime = null;
+    this._times = 1;
+    this._lastTapTime = null;
   }
   componentDidMount() {
-		// 三指连击唤起mdebug
-		const context = this;
-		window.addEventListener('touchend', e => {
-			const { showDebug } = context.state;
-			var nowTime = new Date();
-			var touches = e.touches.length;
-			if (context._times === 1) {
-				context._times++;
-				context._lastTapTime = nowTime;
-				setTimeout(() => {
-					context._times = 1;
-				}, 1000);
-				return;
-			}
-			if (touches === 2 && context._times === 2 && nowTime - context._lastTapTime < 1000) {
-				context._times = 1;
-				context._lastTapTime = new Date();
-				if (!showDebug) {
-					this.setState({
-						showDebug: true
-					});
-				} else {
-					this.setState({
-						showDebug: false
-					});
-				}
-			}
-		});
-	}
+    // 三指连击唤起mdebug
+    const context = this;
+    window.addEventListener('touchend', e => {
+      const { showDebug } = context.state;
+      var nowTime = new Date();
+      var touches = e.touches.length;
+      if (context._times === 1) {
+        context._times++;
+        context._lastTapTime = nowTime;
+        setTimeout(() => {
+          context._times = 1;
+        }, 1000);
+        return;
+      }
+      if (
+        touches === 2 &&
+        context._times === 2 &&
+        nowTime - context._lastTapTime < 1000
+      ) {
+        context._times = 1;
+        context._lastTapTime = new Date();
+        if (!showDebug) {
+          this.setState({
+            showDebug: true,
+          });
+        } else {
+          this.setState({
+            showDebug: false,
+          });
+        }
+      }
+    });
+  }
   render() {
-		const { showDebug } = this.state;
+    const { showDebug } = this.state;
     return (
-      <div className={'mdebug'} style={{
-        display: showDebug ? 'block' : 'none',
-      }}>
-          <MdebugHeader/>
-          <MdebugApplication id={"mdebug-application"}>
-              <h1>Mdebug</h1>
-              <JSONTree data={initData} />
-							<Inspector data={initData} />
-          </MdebugApplication>
-          <MdebugApplication id={"mdebug-system"}>
-            {navigator.userAgent}
-          </MdebugApplication>
+      <div
+        className={'mdebug'}
+        style={{
+          display: showDebug ? 'block' : 'none',
+        }}
+      >
+        <MdebugHeader />
+        <MdebugApplication id={'mdebug-application'}>
+          <h1>Mdebug</h1>
+					<Inspector data={initData} />
+					<h4>文章类型: {initData.content.atype}</h4>
+					<h4>标题: <a href={initData.content.link} target="_blank">{initData.content.title}</a></h4>
+        </MdebugApplication>
+        <MdebugApplication id={'mdebug-system'}>
+          {navigator.userAgent}
+        </MdebugApplication>
       </div>
     );
   }
