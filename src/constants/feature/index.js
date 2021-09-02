ff005f7cf812f353dad3da397e6856497c207212
ihtml5 <phpython8@gmail.com>
2021-09-02 13:28:46 +0800

    feat: add feature check

---
+++ src/constants/feature/index.js
@@ -0,0 +1,50 @@
+export const Features = [
+  {
+    name: 'webGL',
+  },
+  {
+    name: 'remUnit',
+    url: '',
+  },
+  {
+    name: 'viewportUnit',
+    url: '',
+  },
+  {
+    name: 'serviceWorker',
+    url: '',
+  },
+  {
+    name: 'pictureElement',
+    url: '',
+  },
+  {
+    name: 'css3Dtransform',
+    url: '',
+  },
+  {
+    name: 'classList',
+    url: '',
+  },
+  {
+    name: 'matchMedia',
+    url: '',
+  },
+  {
+    name: 'deviceOrientation',
+    url: '',
+  },
+  ,
+  {
+    name: 'deviceMotion',
+    url: '',
+  },
+  {
+    name: 'async',
+    url: '',
+  },
+  {
+    name: 'cors',
+    url: '',
+  },
+];
